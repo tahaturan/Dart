f72ef818c218fd84d5c517315450e3e6f45ab1fe
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-01-08 13:43:45 +0300

    değişkenler ve veri tipleri

--- Bolum_1/hello_world.dart
+++ Bolum_1/hello_world.dart
@@ -3,5 +3,19 @@ void main(List<String> args) {
   print("Taha Turan");
   print("8+5");
   print(8 + 5);
-}
 // ilk dart kodum
+
+  // Veri Tipkeri Ve Değişkenler
+
+  int sayi = 15;
+  String isim = ("taha");
+  bool dogru_mu = true;
+// bide bunların hepsini kapsayan yani değişkenin hangi türde oldugunu anlayan ifademiz var
+
+  var okul_no = 12478;
+//int oldugunu anladı
+  var soyad = ("turan");
+  var numara = 12354;
+  print(soyad);
+  print(numara);
+}

d239d6110135110f9a55aa46d0484c3932c412e8
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-01-08 14:14:33 +0300

    veri dönüşümleri

--- Bolum_1/hello_world.dart
+++ Bolum_1/hello_world.dart
@@ -10,12 +10,19 @@ void main(List<String> args) {
   int sayi = 15;
   String isim = ("taha");
   bool dogru_mu = true;
-// bide bunların hepsini kapsayan yani değişkenin hangi türde oldugunu anlayan ifademiz var
 
+  int kilo = 86.8
+      .toInt(); // toInt() metodu sayıyı int yani tam sayı yapmamızı sağlıyor
+
+// bide bunların hepsini kapsayan yani değişkenin hangi türde oldugunu anlayan ifademiz var
   var okul_no = 12478;
 //int oldugunu anladı
   var soyad = ("turan");
   var numara = 12354;
   print(soyad);
   print(numara);
+
+  double en = 10;
+  double boy = 12;
+  print("eni : ${en.toInt()} boyu:${boy.toInt()} olan diktörgenin alanı: ${(en * boy).toInt()} dir");
 }

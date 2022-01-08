f623df1c85fb0a6d8694017ab831822a6092637a
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-01-08 14:32:19 +0300

    uygulama 1

---
+++ Bolum_1/uygulama1.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  String isim = ("taha");
+  var soyIsim = ("Turan");
+  int yas = 24;
+  print(
+      "Benim Adım $isim $soyIsim yaşım $yas tüm ismimdeki toplam karakter sayisi ${(isim.length) + (soyIsim.length)}");
+  //************************** */
+  //üçgen Çevresi Uygulaması
+  var kenar1 = 3;
+  var kenar2 = 4;
+  var kenar3 = 5;
+
+  var cevre = kenar1 + kenar2 + kenar3;
+  print("Çevre: $cevre");
+}

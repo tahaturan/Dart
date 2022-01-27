417ca06eecd5bb98b25b10348abe77374dc25bd0
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-01-28 02:45:33 +0300

    Alan_Hesabi

---
+++ bolum2/Alan_Hesabi.dart
@@ -0,0 +1,28 @@
+import 'dart:ffi';
+import 'dart:io';
+
+void main(List<String> args) {
+  print("1-) Dikdörtgen Alanı");
+  print("2-) Çember Alani");
+  print("Seçminizi Yapınız: ");
+
+  int secim = int.parse(stdin.readLineSync()!);
+
+  if (secim == 1) {
+    print("Seçimiz: Dikdörtgen alanı ");
+    print("Kısa Kenarı Giriniz: ");
+    int kisaKenar = int.parse(stdin.readLineSync()!);
+    print("Uzun Kenarı Giriniz: ");
+    int uzunKenar = int.parse(stdin.readLineSync()!);
+    int alan = kisaKenar * uzunKenar;
+    print("Dikdörtgenin alanı: $alan");
+  } else if (secim == 2) {
+    print("Seçimizi: Çember alanı ");
+    print("Yarı Çapı Giriniz");
+    int yariCap = int.parse(stdin.readLineSync()!);
+    var cemberAlan = (3.14 * 3.14) * yariCap;
+    print("Çemberin Alanı: $cemberAlan");
+  } else {
+    print("Hatali Seçim");
+  }
+}

e7aa26c6ffe7ab292c245d096a04cf72771a085a
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-01-30 00:34:54 +0300

    tekrar uygulaması günler

--- bolum2/switch_kullanimi.dart
+++ bolum2/switch_kullanimi.dart
@@ -29,4 +29,35 @@ void main(List<String> args) {
       }
   }
 
+  print("*****************");
+
+  int gun = 5;
+  switch (gun) {
+    case 1:
+      print("Pazartesi");
+      break;
+    case 2:
+      print("Salı");
+      break;
+    case 3:
+      print("Çarşamba");
+      break;
+    case 4:
+      print("Perşembe");
+      break;
+    case 5:
+      print("Cuma");
+      break;
+    case 6:
+      print("Cumartesi");
+      break;
+    case 7:
+      print("Pazar");
+      break;
+    default:
+      {
+        print("farklı değer...");
+        break;
+      }
+  }
 }

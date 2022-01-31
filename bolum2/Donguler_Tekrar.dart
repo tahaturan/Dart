6e8a7375d293aca1f741941e9091c1ec53a07167
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-01-31 03:14:44 +0300

    sonsuz döngü tek-çift sayı bulma

--- bolum2/Donguler_Tekrar.dart
+++ bolum2/Donguler_Tekrar.dart
@@ -16,12 +16,29 @@ void main(List<String> args) {
   for (var i = 1; i <= tekrar; i++) {
     print("$i-) $isim");
   }
-    //veri işleme while
+  //veri işleme while
 
   print("İşlenecek Veri Miktarını Giriniz: ");
   int veriSayisi = int.parse(stdin.readLineSync()!);
   while (veriSayisi > 0) {
     print("$veriSayisi. Veri");
     veriSayisi--;
+  }
+
+  //tek çift sayılar
+
+  while (true) {
+    print("Sayı Giriniz Çımak için(1) e basınız : ");
+    int sayi = int.parse(stdin.readLineSync()!);
+    if (sayi == 1) {
+      print("Çıkış Yapıldı...");
+      break;
+    } else {
+      if (sayi % 2 == 0) {
+        print("Çift Sayı");
+      } else {
+        print("Tek Sayı");
+      }
+    }
   }
 }

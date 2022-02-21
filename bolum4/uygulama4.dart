6d8a2f62e6131dce0e8697008852147168ba3473
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-02-21 10:13:37 +0300

    tekrar eden for kaldırıldı

--- bolum4/uygulama4.dart
+++ bolum4/uygulama4.dart
@@ -2,12 +2,9 @@ import 'dart:math';
 
 void main(List<String> args) {
   List<int> liste1 = List.filled(5, 0);
-
-  for (var i = 0; i < liste1.length; i++) {
-    liste1[i] += Random().nextInt(50);
-  }
   List<int> liste2 = List.filled(5, 0);
   for (var i = 0; i < liste2.length; i++) {
+    liste1[i] += Random().nextInt(50);
     liste2[i] += Random().nextInt(50);
   }
 

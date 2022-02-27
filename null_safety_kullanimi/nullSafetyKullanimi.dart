56bc8b93e77f2c666537595353a454390ab8baa6
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-02-27 16:08:46 +0300

    nul lSafety Kullanimi

---
+++ null_safety_kullanimi/nullSafetyKullanimi.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  String? mesaj = null;
+  //değişkenin null olabiliğini söylüyoruz
+  String? mesaj2;
+  //bu şekilde de kullanabiliriz
+
+  print("sonuc 1: ${mesaj?.toUpperCase()}");
+  //buraya ? işareti koyarsak null değer gelebilir ona ragmen calıstır demek
+  mesaj = "merhaba";
+  print("sonuc: ${mesaj.toUpperCase()}");
+
+  //* diğer yöntem ! kullanımı
+  //print("sonuc2: ${mesaj2!.toUpperCase()}");
+  //burada ise ben kodlamama güveniyorum null diye bir değer gelmicek demek
+  //!null değer gelirse uygulama çöker!!
+
+  //* Diğer yöntem if kontrolü
+  if (mesaj2 != null) {
+    print("Null değil");
+  } else {
+    print("Null değer");
+  }
+}

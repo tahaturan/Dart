3d198826fc55778a41333a7f195956122e602bfa
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-02-27 23:44:54 +0300

    Erisim

---
+++ bolum5/tekrar/erisim.dart
@@ -0,0 +1,4 @@
+class Erisim {
+  late int publicDegisken; //! dışarıdan erişebilir
+  late int _privateDegisken; //! Dışarıdan erişilemez sadece olusturuldugu sınıftan erişim sağlayabiliriz
+}
---
+++ bolum5/tekrar/erisimMain.dart
@@ -0,0 +1,8 @@
+import 'erisim.dart';
+
+void main(List<String> args) {
+  var nesne = Erisim();
+  nesne.publicDegisken = 10; // nesne. dediğimizde public değişkene erişebiliyoruz 
+  //! nesne. diyerek private değişekene erişemiyoruz!!
+  
+}

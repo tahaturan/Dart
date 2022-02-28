a975301d8b0ba07cb2815660ca8fbdba9c149ab5
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-03-01 00:41:13 +0300

    Main bölümü olusturuldu

---
+++ bolum5/polyMorphism/main.dart
@@ -0,0 +1,23 @@
+import 'isci.dart';
+import 'mudur.dart';
+import 'ogretmen.dart';
+import 'personel.dart';
+
+void main(List<String> args) {
+  var mudur = Mudur();
+
+  //! İlk olarak SuperClass-Nesne İsmi = SubClass şeklinde olusturulur
+
+  Personel ogretmen = Ogretmen();
+  //* işte burada polymorphism i uygulamıs oluyoruz personel türünde nesne olusturduk fakat davranısı ogretmen sınıfına göre
+  Personel isci = Isci();
+  //* Yine personel sınıfı türünden nesne olusturduk fakat isci sınıfı gibi davranmasını istedik
+
+  mudur.iseAl(ogretmen);
+  print("*************");
+  mudur.iseAl(isci);
+
+  //* mudur sınıfındaki iseAl metodu içine parametre olarak personel sınıfından olan bir nensne istediği için biz ogretmen ve işçi nesneleriimizi olustururken polymorphisim i kullanık yani çok şekillik personel veri tipinde ama davranısı ogretmen sınıfına ait
+
+  
+}

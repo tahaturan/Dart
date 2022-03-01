5ad42d89000a0656661f4462ff969c0d10e86e78
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-03-02 01:13:33 +0300

    Amasya elması sınıfı olusturuldu

---
+++ bolum5/Interface_Kullanimi/uygulama1/amasyaElmaClass.dart
@@ -0,0 +1,13 @@
+import 'elmaClass.dart';
+
+class AmasyaElmasi extends Elma {
+  @override
+  void howToEat() {
+    print("Amasya elması yenebilir");
+  }
+
+  @override
+  void howTosqueeze() {
+    print("Amasya elması Sıkılabilir");
+  }
+}

ba9b80728d5b9937ebd75fa34dc31a19be855583
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-03-02 01:17:40 +0300

    kalıtım oldugu için gereksiz kodlar silindi

--- bolum5/Interface_Kullanimi/uygulama1/amasyaElmaClass.dart
+++ bolum5/Interface_Kullanimi/uygulama1/amasyaElmaClass.dart
@@ -1,13 +1,5 @@
 import 'elmaClass.dart';
 
 class AmasyaElmasi extends Elma {
-  @override
-  void howToEat() {
-    print("Amasya elması yenebilir");
-  }
 
-  @override
-  void howTosqueeze() {
-    print("Amasya elması Sıkılabilir");
-  }
 }

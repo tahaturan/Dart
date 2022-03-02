9728c7146c1c378fd0b35907434b2a07d854bae6
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-03-02 19:21:55 +0300

    implements kavramı

--- bolum5/Interface_Kullanimi/InterfaceKullanimi.dart
+++ bolum5/Interface_Kullanimi/InterfaceKullanimi.dart
@@ -16,3 +16,5 @@ class ClassA implements Interface1 {
 }
 
 // olusturudugumuz Interface yapısı bir nevi taslak yapı olarak düşünebiliriz 
+//! Normalde kalıtımda bir sınıfa birden fazla kaalıtım eklemeiyoruz yani interface kavramı yokrur dart dilinde 
+//! bunun önğne geçmek içinde implements i kullanıyoruz böyleelikle birden fazla soyut (abstract) sınıfı bir sınıfa dahil edebiliyoruz

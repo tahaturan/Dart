b5ce6f0b07a2017428d3c7a4ae78c4a448a0dbce
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-03-03 06:16:53 +0300

    Sıralama işlemi Comparator

--- collections_tekrar/list_nesne_tabanli_calisma/main.dart
+++ collections_tekrar/list_nesne_tabanli_calisma/main.dart
@@ -9,8 +9,34 @@ void main(List<String> args) {
   ogrenciler.add(ogrenci1);
   ogrenciler.add(ogrenci2);
   ogrenciler.add(ogrenci3);
+  listeYazdir(ogrenciler);
+  print("******************");
 
-  for (var i in ogrenciler) {
+  //* Liste Sıralama İşlemleri
+  Comparator<Ogrenciler> siralama1 = (x, y) => x.no.compareTo(y.no);
+  ogrenciler.sort(siralama1);
+  listeYazdir(ogrenciler);
+  //todo Burada Küçüten Büyüğe Bir Sıralama yaptık liste içinde
+  // büyükten küçüğe sıralama
+  print("**********büyükten küçüğe sıralama******");
+  Comparator<Ogrenciler> siralama2 = (y, x) => x.no.compareTo(y.no);
+  ogrenciler.sort(siralama2);
+  listeYazdir(ogrenciler);
+
+  //todo Yukarıda yaptıgımız sıralamalar ogreci no suna göre sıralamaydı istersek ada görede sıralama yapabiliriz
+  print("***********Ad a göre sıralama*********");
+  Comparator<Ogrenciler> siralamaAd = (x, y) => x.ad.compareTo(y.ad);
+  ogrenciler.sort(siralamaAd);
+  listeYazdir(ogrenciler);
+  //todo Büyük Küçüğe Liste içindeki öğrencileri ad a göre sıraladı birde küçüten büyüğe şeklini yapalım
+  print("Büyükten Küçüğe Sıralama ad a göre");
+  Comparator<Ogrenciler> siramalaBuyukAd = (x, y) => y.ad.compareTo(x.ad);
+  ogrenciler.sort(siramalaBuyukAd);
+  listeYazdir(ogrenciler);
+}
+
+void listeYazdir(List ogrenci) {
+  for (var i in ogrenci) {
     print("ad: ${i.ad}\nNo: ${i.no}\nSınıf: ${i.sinif}");
     print("--------------");
   }
